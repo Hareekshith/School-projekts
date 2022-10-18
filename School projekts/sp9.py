6b075b5994e49ca2b17bd48d7305b75ff0abf734
Hareekshith <83201177+Hareekshith@users.noreply.github.com>
2022-10-18 10:52:44 +0530

    Update sp9.py

--- School projekts/sp9.py
+++ School projekts/sp9.py
@@ -1,25 +1,13 @@
 n = int(input("Enter a number: "))
 n1 = int(input("Enter a number: "))
-factors = list()
-factors1 = list()
-HCF = list()
-LCM = list()
+hcf = 1
+lcm = 1
 
 for i in range(1, n+1):
-    if n%i==0:
-        factors.append(i)
+    if n%i == 0 and n1%i == 0:
+        hcf = i
+print(f"The HCF of {n} and {n1} is {HCF}")
 
-for i1 in range(1, n1+1):
-    if n1%i1 == 0:
-        factors.append(i1)
+lcm = n*n1/hcf
 
-for i in factors:
-    if i in factors1:
-        HCF.append(i)
-
-for i in range(1, n*n1+1):
-    if i%n==0 and i%n1==0:
-        LCM.append(i)
-
-print("HCF: ", sum(HCF))
-print("LCM: ", sum(LCM))
+print(f"The LCM of {n} and {n1} is {LCM}")
